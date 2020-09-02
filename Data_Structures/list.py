4830e7482e6e243d2d6b96f850d5332e20dc452a
tawabshakeel <tawab2013@gmail.com>
2020-09-02 21:02:32 +0500

    list added

---
+++ Data_Structures/list.py
@@ -0,0 +1,14 @@
+a_lst = [2200, 2350, 2600, 2130, 2190]
+
+a = a_lst[1] - a_lst[0]
+
+b = a_lst[0] + a_lst[1] + a_lst[2]
+
+a_lst[5] = 1980
+
+a_lst[3] = a_lst[3] - 200
+
+# searching element by index
+### big0 = 0(1)
+# searching index by element
+### big0 0(n)
